313a7768d4f714ed964521c9cdfef5bc91a9b263
karishma27994 <joshikarishma2409@gmail.com>
2025-11-22 23:33:32 +0530

    Amend Changes- 22/11/2025 23:33 PM

---
+++ src/questions/programs/TransposeMatrix.java
@@ -0,0 +1,44 @@
+package questions.programs;
+
+import java.util.Scanner;
+
+public class TransposeMatrix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int arr[][] = new int[n][m];
+
+        //input values in an array
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        //Printing original array
+        System.out.println("Original Matrix:");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        //Printing transpose array
+        System.out.println("Transpose Matrix:");
+        //Logic 1 to transpose matrix
+       /* for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(arr[j][i] + " ");
+            }
+            System.out.println();
+        }*/
+        // Logic 2 to transpose matrix
+        for (int j = 0; j < m; j++) {
+            for (int i = 0; i < n; i++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
